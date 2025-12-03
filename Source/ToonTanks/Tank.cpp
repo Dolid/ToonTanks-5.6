9c377b6a27e7a573511ddebea294d3f2b306a754
lid <804648466@qq.com>
2025-12-04 01:49:39 +0800

    Initial commit

---
+++ Source/ToonTanks/Tank.cpp
@@ -0,0 +1,76 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Tank.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "Components/InputComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "DrawDebugHelpers.h"
+ATank::ATank()
+{
+    SpringArm =  CreateDefaultSubobject<USpringArmComponent>(TEXT("Spring Arm"));
+    SpringArm->SetupAttachment(RootComponent);
+
+    Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+    Camera->SetupAttachment(SpringArm);
+}
+
+// Called to bind functionality to input
+void ATank::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	PlayerInputComponent->BindAxis(TEXT("MoveForward"), this, &ATank::Move);
+	PlayerInputComponent->BindAxis(TEXT("Turn"), this, &ATank::Turn);
+}
+
+// Called every frame
+void ATank::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+    if(PlayerControllerRef)
+    {
+        FHitResult HitResult;
+        PlayerControllerRef->GetHitResultUnderCursor(
+            ECollisionChannel::ECC_Visibility,
+            false,
+            HitResult
+         );
+        DrawDebugSphere(
+            GetWorld(),
+            HitResult.ImpactPoint,
+            25.f,
+            12,
+            FColor::Red,
+            false,
+            -1.f
+        );
+    }
+}
+
+// Called when the game starts or when spawned
+void ATank::BeginPlay()
+{
+	Super::BeginPlay();
+	// PlayerControllerRef = (APlayerController*)GetController();  
+	PlayerControllerRef = Cast<APlayerController>(GetController());
+}
+
+void ATank::Move(float Value)
+{
+    UE_LOG(LogTemp, Warning, TEXT("Move called Value at %f one tick"), Value);
+
+	// FVector DeltaLocation(0.f);
+    FVector DeltaLocation = FVector::ZeroVector;
+    float DeltaTime = UGameplayStatics::GetWorldDeltaSeconds(this);
+    DeltaLocation.X = Value * Speed * DeltaTime;
+	AddActorLocalOffset(DeltaLocation, true);
+}
+
+void ATank::Turn(float Value)
+{
+    FRotator  DeltaRotation  = FRotator::ZeroRotator;
+    float DeltaTime = UGameplayStatics::GetWorldDeltaSeconds(this);
+    DeltaRotation.Yaw = Value * DeltaTime * TurnRate;
+    AddActorLocalRotation(DeltaRotation, true);
+}
