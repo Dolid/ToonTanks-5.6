58e61d54aa0aa60ef763f615b0d4c884564b56ca
lid <804648466@qq.com>
2025-12-09 00:37:12 +0800

    Timer

--- Source/ToonTanks/Tower.h
+++ Source/ToonTanks/Tower.h
@@ -26,4 +26,12 @@ private:
 
 	UPROPERTY(EditDefaultsOnly, Category = "Combat")
 	float FireRange = 1000.f;
+
+	FTimerHandle FireRateTimerHandle;
+
+	float FireRate = 2.f;
+
+	void CheckFireCondition();
+
+	bool InFireRange();	
 };
